a9b44d79b037abfb3757cc852270ecbb48d2e15b
Nico Hämäläinen <nibsivaan@gmail.com>
2024-01-29 08:03:26 +0200

    Progress

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// https://fonts.google.com/specimen/Inter?query=inter
+export const inter = Inter({ subsets: ['latin'] });
+
+// https://fonts.google.com/specimen/Lusitana?query=lusitana
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
